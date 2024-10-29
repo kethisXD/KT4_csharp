10b7a6132ead214abe233d18e769704a022a29e8
Sergey <KharitonovSS22@st.ithub.ru>
2024-10-29 13:39:47 +0300

    Done

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿using FileKT4libary;
+
+string path_file = @"C:\Users\xxx\projects\csharp\libary\KT4_main\text.txt";
+FileKT my_file = new FileKT(path_file);
+
+
+my_file.WriteFile("Hello from main");
+my_file.ReadFile();
